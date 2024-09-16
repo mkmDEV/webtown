8c6a2686f3028f355652f78edd32add866351464
mkmDEV <mkmdev18@gmail.com>
2024-09-16 13:25:52 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
+      distinctio, dolores pariatur qui quis reiciendis rerum. A adipisci aperiam
+      consequuntur, doloremque fugit nam officiis provident qui, repellat sunt,
+      veritatis voluptates!
+    </>
+  );
+}
+
+export default App;
